411f6c0bfeaee0e073f4c331379ed2da528e5281
SotskovS <svalgele@gmail.com>
2021-01-18 07:32:46 +0300

    news: create, editing. add devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
+
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+         
   has_many :events
 
   validates :name, presence: true, length: {maximum: 35}
